032fdc2e3dccbe08833ccb70bf19894f3d0a5d9d
Elvis Porebski <eporebski@zendesk.com>
2021-05-27 13:32:41 +0100

    Tagging and wiring
    - Enrich parsed test results with the provided tags
    - Wire things together
    - Update GA inputs & README

--- dd-test-results/src/main.ts
+++ dd-test-results/src/main.ts
@@ -1,3 +1,26 @@
 import * as junitTestResultParser from './junit-test-result-parser'
+import core from '@actions/core'
+import {tagTestResults} from './tagging'
+import {DataDogClient, Metric} from './client'
 
-console.log(junitTestResultParser.parse('test.xml'))
+function buildMetrics(taggedTestCases: TaggedTestCase[]): Metric[] {
+  console.log(taggedTestCases)
+  return []
+}
+
+async function main(
+  junitTestResultsFile: string,
+  tagsFile: string,
+  client: DataDogClient
+): Promise<void> {
+  const testResults = await junitTestResultParser.parse(junitTestResultsFile)
+  const taggedTestCases = tagTestResults(testResults, tagsFile)
+  const metrics = buildMetrics(taggedTestCases)
+  client.sendMetrics(metrics)
+}
+
+main(
+  core.getInput('junit-test-results', {required: true}),
+  core.getInput('test-results-tags', {required: true}),
+  new DataDogClient(core.getInput('dd-api-key', {required: true}))
+)
---
+++ dd-test-results/tests/tagging.test.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import {tagTestResults} from '../src/tagging'
+
+describe('tagging', () => {
+  it('should decorate test results with tags', () => {
+    const testCase = {
+      name: 'sampleTestName',
+      duration: 0,
+      result: 'succeeded'
+    }
+    const testResults = {
+      testSuites: [
+        {
+          name: 'com.example.SampleTests',
+          testCases: [testCase]
+        }
+      ]
+    }
+
+    const tagsFile = path.join(__dirname, 'resources', 'test-results-tags.json')
+    const taggedTestCases = tagTestResults(testResults, tagsFile)
+
+    expect(taggedTestCases).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          ...testCase,
+          tags: {
+            team: 'c',
+            test_case: 'sampleTestName',
+            test_classification: 'smoke',
+            test_suite: 'sample_tests'
+          }
+        })
+      ])
+    )
+  })
+})
