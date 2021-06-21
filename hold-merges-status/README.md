1851e92ac6ed56f6e282a53cc9969b5d9a68cb17
Creighton Long <clong@zendesk.com>
2021-06-21 12:56:49 -0500

    Make 'hold' fail by default; add request timeout; add 'error' status option

--- hold-merges-status/README.md
+++ hold-merges-status/README.md
@@ -14,8 +14,8 @@ Check with the hold-merges app to see if merging is currently allowed
 ## Inputs
 
 - `repo`: The name of the repo to check in hold-merges. Default: $GITHUB_REPOSITORY
-- `fail_on_hold_status`: Fail the check if the status from hold-merges is `hold`. Default: false
+- `fail_on_hold_status`: Fail the check if the status from hold-merges is `hold`. Set `false` to keep this informative (not recommended). Default: `true`.
 
 ## Outputs
 
-- `status`: The current merge status. Either "hold" or "allow".
+- `status`: The current merge status. Either "hold", "allow", or "error".
--- hold-merges-status/action.js
+++ hold-merges-status/action.js
@@ -6,7 +6,7 @@ const HOLD_MERGES_BASE_URL = 'http://hold-merges.internaltools-staging-use1.zend
 
 async function fetchStatus() {
   const repo = core.getInput('repo') || process.env['GITHUB_REPOSITORY']
-  const client = new httpm.HttpClient('hold-merges-action')
+  const client = new httpm.HttpClient('hold-merges-action', undefined, { socketTimeout: 30000 });
   const response = await client.get(`${HOLD_MERGES_BASE_URL}/status/${repo}`)
   const body = await response.readBody()
   const json = JSON.parse(body)
@@ -20,7 +20,7 @@ async function run() {
   core.info(`Hold-merges status: ${status}`)
   core.setOutput('status', status)
 
-  if (core.getInput('fail_on_hold_status')) {
+  if (status == 'hold' && core.getInput('fail_on_hold_status') != 'false') {
     core.setFailed('Hold status detected. Failing check.')
   }
 }
@@ -28,5 +28,6 @@ async function run() {
 try {
   run()
 } catch (error) {
+  core.setOutput('status', 'error')
   core.setFailed(`Action failed with error ${error}`)
 }
